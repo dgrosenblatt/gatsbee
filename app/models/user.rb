c08c6f9a433fd49f3702efae78674f21a15c948e
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-01-03 23:44:30 -0500

    Fix user sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   def self.from_omniauth(auth)
-    User.find_by(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+    User.where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
