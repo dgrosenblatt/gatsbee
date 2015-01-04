8bcea3244f8798238dc4bd32403e7702aefc156d
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-01-03 23:25:58 -0500

    Add sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get "/auth/:provider/callback" => 'sessions#create'
+
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 end
