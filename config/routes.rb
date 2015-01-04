0cdc8d6792ebbce7f37b47532115db678de235e5
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-01-03 23:19:19 -0500

    Add users and omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get "/auth/:provider/callback" => 'sessions#create'
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :user do
+    provider "MyString"
+uid "MyString"
+name "MyString"
+oauth_token "MyString"
+oauth_expires_at "2015-01-03 23:03:47"
+role "MyString"
+favorite_authors "MyString"
+favorite_books "MyString"
+  end
+
+end
