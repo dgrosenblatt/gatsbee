96c5dae0c1568cd79a6843ea23e7cb85e58003b6
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-01-04 10:35:40 -0500

    Add omniauth test helper and sign in test

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+feature "Signing in with google" do
+  scenario "from the home page" do
+    set_omniauth()
+    visit "/"
+    click_link "Sign in"
+
+    expect(page).to have_content "Sign Out"
+    expect(page).to have_content "Profile"
+    expect(page).to have_content "Clubs"
+    expect(page).not_to have_link "Sign in"
+
+  end
+end
