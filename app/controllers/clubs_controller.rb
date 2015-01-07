a177e0ff80c06d4b27bd079730c26f8e22eb36b4
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-01-07 18:05:09 -0500

    Add club show pages

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,7 +1,12 @@
 class ClubsController < ApplicationController
+  before_action :authenticate_user, only: [:show]
 
   def index
     @clubs = Club.all
+  end
+
+  def show
+    @club = Club.find(params[:id])
   end
 
 end
---
+++ spec/features/club_show_page_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+feature "Viewing a public club's page" do
+  scenario "while signed in" do
+    club = FactoryGirl.create(:club)
+    user = FactoryGirl.create(:user)
+    set_omniauth
+    visit "/"
+    click_link "Sign in"
+    visit club_path(club)
+
+    expect(page).to have_content "Book Club"
+    expect(page).to have_content "A book club for all the cool programmers"
+  end
+
+  scenario "without signing in" do
+    club = FactoryGirl.create(:club)
+    visit club_path(club)
+
+    expect(page).to have_content "Please sign in with Google to continue"
+    expect(page).not_to have_content "Book Club"
+    expect(page).not_to have_content "A book club for all the cool programmers"
+  end
+end
