80092c534ccdbaf2ad93e393490b2d1a614fb9e0
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-01-14 17:35:50 -0500

    Handle zero result set from Amazon api

--- app/models/amazon_api.rb
+++ app/models/amazon_api.rb
@@ -5,7 +5,10 @@ class AmazonApi
     response = request.item_search(
       query: { "Keywords"=>book.title, "SearchIndex"=>"Books" }
     )
-    results = response.parse["ItemSearchResponse"]["Items"]["Item"].first #array
+    results = response.parse["ItemSearchResponse"]["Items"]["Item"].try(:first)
+    if results.nil?
+      return
+    end
     book.amazon_url = results["DetailPageURL"]
     book.update_attributes(image_lookup(results["ASIN"]))
   end
@@ -13,10 +16,8 @@ class AmazonApi
   def self.image_lookup(asin)
     request = Vacuum.new
     request.associate_tag = ENV["ASSOCIATE_TAG"]
-    response = request.item_lookup(
-      query: { "ItemId"=>asin, "ResponseGroup"=>"Images" }
-    )
+    response = request.item_lookup(query: { "ItemId"=>asin, "ResponseGroup"=>"Images" })
     { small_image: response.parse["ItemLookupResponse"]["Items"]["Item"]["SmallImage"]["URL"],
-      medium_image: response.parse["ItemLookupResponse"]["Items"]["Item"]["MediumImage"]["URL"]}
+      medium_image: response.parse["ItemLookupResponse"]["Items"]["Item"]["MediumImage"]["URL"] }
   end
 end
