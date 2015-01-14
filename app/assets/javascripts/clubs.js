46485fc4f8e24e97a95f0419a50714e44618fd04
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-01-14 17:36:39 -0500

    Fix div height issue with js executing before image load

--- app/assets/javascripts/clubs.js
+++ app/assets/javascripts/clubs.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(window).load(function(){
   var height = Math.max($( ".club-page-right" ).height(), $( ".club-page-left" ).height());
   $( ".club-page-left" ).height(height);
   $( ".club-page-right" ).height(height);
