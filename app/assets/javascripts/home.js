8d242ca92d35e6f1d88d33d3d8897d20fdd2a9f8
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-01-17 23:13:59 -0500

    Fix home page arrow fading issue

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
       opacity: 1,
       height: "+=25"
     }, 100, function(){
-      $(this).find(".fa-arrow-circle-down").fadeIn();
+      $(this).find(".fa-arrow-circle-down").fadeIn(400);
     });
   });
 
@@ -16,7 +16,16 @@ $(document).ready(function(){
       height: "-=25"
     }, 100, function(){
       $(this).css("margin-bottom", "50px");
+      $(this).find(".fa-arrow-circle-down").fadeOut(10);
     });
+  });
+
+  $(".stories p").change(function(){
+    console.log("a change occurred")
+    if ($(this).height() == 175) {
+
+      $(this).find(".fa-arrow-circle-down").fadeOut(100);
+    }
   });
 
   $(".stories p#discover").click(function() {
@@ -30,7 +39,7 @@ $(document).ready(function(){
       scrollTop: $("#story2").offset().top
     }, 750);
   });
-  
+
   $(".stories p#engage").click(function() {
     $('html, body').animate({
       scrollTop: $("#story3").offset().top
