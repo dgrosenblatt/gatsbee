9fb86975a9bc27e97385207e58c04d6d24eca39d
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-01-21 18:18:44 -0500

    Change google sign in settings

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,5 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'],
   { access_type: 'offline',
-    scope: 'userinfo.email, calendar',
-    prompt: 'consent' }
+    scope: 'userinfo.email, calendar' }
 end
