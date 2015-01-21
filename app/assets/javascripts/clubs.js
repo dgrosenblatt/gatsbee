8f89a4ef62b8979417b67238c05dccd6b24d39fa
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-01-21 15:02:27 -0500

    Club show page button and form styling

--- app/assets/javascripts/clubs.js
+++ app/assets/javascripts/clubs.js
@@ -1,9 +1,21 @@
 $(window).load(function(){
-  var height = Math.max($( " .club-page-right " ).height(), $( " .club-page-left " ).height());
-  $( " .club-page-left " ).height(height);
-  $( " .club-page-right " ).height(height);
+  var height = Math.max($(".club-page-right").height(), $(".club-page-left").height());
+  $(".club-page-left").height(height);
+  $(".club-page-right").height(height);
 
-  $(" #club-discussion ").click(function(){
-    $(" .new_comment ").slideToggle();
+  $("#club-discussion").click(function(){
+    $(".new_comment").slideToggle();
+  });
+
+  $("#meeting-time-info").click(function(){
+    $("#meeting-instructions").slideToggle();
+  });
+
+  $("#meeting-instructions p").click(function(){
+    $("#meeting_meeting_time").val($(this).text());
+  });
+
+  $("#club-meetings").click(function(){
+    $("#meetings-form").slideToggle();
   });
 });
