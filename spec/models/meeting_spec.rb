09e3b8e267d00d5870898d1028aa54a6b7ef5db6
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-01-23 15:58:16 -0500

    Fix failing meeting model test

--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Meeting, :type => :model do
   it { should have_valid(:location).when("Location") }
   it { should_not have_valid(:location).when(*blanks) }
 
-  it { should have_valid(:meeting_time).when("Time") }
+  it { should have_valid(:meeting_time).when("Tomorrow at noon") }
   it { should_not have_valid(:meeting_time).when(*blanks) }
 
   it { should belong_to(:user) }
